a45a690ea9e1ba6ddeb44f4485124761315f3929
generous-program-0200 <harrycopley@vultuslimited.com>
2018-12-08 16:29:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  greetings = []
+  names.each do |name|
+    names << badge_maker(name)
+  end
+  greetings
+end
+
+#rooms 1 - 7
+def assign_rooms(speakers)
+  greeting[]
+  speakers.each do |speaker, index|
+  greeting << "Hello, #{speakers} You'll be assigned to room #{index+1}!"
+  greeting
+end
+end
+
+def printer(people)
+  result1 = batch_badge_creator(people)
+  result1.each do |x|
+    puts x
+  end
+  result = assign_rooms(people)
+  result.each do |x|
+    puts x
+  end
+end
+
